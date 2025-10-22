977c12dcd0b3806f4876c03ed95949d3e2cd0448
Jonathan Warburton <jonathan.r.warburton@leidos.com>
2025-10-22 15:11:38 -0400

    [3]: Initial login page code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component'; // <- Import it
 
 @Component({
   selector: 'app-root',
-  imports: [LoginComponent],
+  standalone: true,
+  imports: [RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
